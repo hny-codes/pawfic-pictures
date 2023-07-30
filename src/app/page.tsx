9eefe4e9ae55f743ea242acfb718427194345438
Henry <hnguyhenry@protonmail.com>
2023-07-29 17:38:26 -0700

    Adjust text size for desktop viewport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
           className='animate-dogHero max-h-[65vh] sm:max-h-max sm:order-1 sm:h-screen object-cover '
         />
         <div className='text-[--clr-text] text-center mt-8 px-8 sm:place-self-center sm:text-left'>
-          <h1 className='italic text-3xl'>Love Dogs?</h1>
-          <h2 className='text-sm mt-4'>We do too! Get your dog dosage here</h2>
+          <h1 className='italic text-3xl sm:text-6xl'>Love Dogs?</h1>
+          <h2 className='text-sm sm:text-lg mt-4'>We do too! Get your dog dosage here</h2>
           <Link href={'/login'}>
             <button className='mt-8 bg-[--clr-primary-01] px-4 py-2 rounded-lg hover:opacity-80 transition'>
               Join us!
