2dca33fab72717690e1f4d2deea184a8763594b5
Henry <hnguyhenry@protonmail.com>
2023-07-29 17:24:48 -0700

    Add desktop view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,13 @@ import DogPic from 'public/images/dog.jpg';
 export default function Home() {
   return (
     <main>
-      <div>
-        <Image src={DogPic} alt='dog picture' className='animate-dogHero' />
-        <div className='text-[--clr-text] text-center mt-8 px-8'>
+      <div className='sm:grid sm:grid-cols-2'>
+        <Image
+          src={DogPic}
+          alt='dog picture'
+          className='animate-dogHero sm:order-1 sm:h-screen sm:object-cover '
+        />
+        <div className='text-[--clr-text] text-center mt-8 px-8 sm:place-self-center sm:text-left'>
           <h1 className='italic text-3xl'>Love Dogs?</h1>
           <h2 className='text-sm mt-4'>We do too! Get your dog dosage here</h2>
           <Link href={'/login'}>
