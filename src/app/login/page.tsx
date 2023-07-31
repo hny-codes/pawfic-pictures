42a3e2a83076be65f9f616266a26d32917ab6be6
Henry <hnguyhenry@protonmail.com>
2023-07-30 19:31:55 -0700

    Add try catch for dupe emails

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
-import Link from 'next/link';
 import DogPic from 'public/images/dog.jpg';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import {
@@ -26,6 +25,7 @@ const ErrorMissing = ({ element }: { element: string }) => (
 
 export default function Login() {
   const [loading, setLoading] = useState(false);
+  const [accError, setAccError] = useState(false);
 
   const {
     register,
@@ -45,35 +45,50 @@ export default function Login() {
     breed: string
   ) => {
     const userId = ID.unique();
-    // Auth user creation
-    let userResponse = await account.create(userId, email, password, username);
 
-    // Document creation
-    let payload = {
-      email,
-      userId: userResponse.$id,
-      username,
-      password,
-      fav_breed: breed,
-    };
+    try {
+      // Auth user creation
+      let userResponse = await account.create(
+        userId,
+        email,
+        password,
+        username
+      );
 
-    let documentResponse = await database.createDocument(
-      USERS_DATABASE,
-      USERS_COLLECTION,
-      ID.unique(),
-      payload
-    );
+      // Document creation
+      let payload = {
+        email,
+        userId: userResponse.$id,
+        username,
+        password,
+        fav_breed: breed,
+      };
 
-    // console.log('USER');
-    // console.log(userResponse);
+      let documentResponse = await database.createDocument(
+        USERS_DATABASE,
+        USERS_COLLECTION,
+        ID.unique(),
+        payload
+      );
 
-    // console.log('DOCUMENT');
-    // console.log(documentResponse);
+      console.log('USER');
+      console.log(userResponse);
+
+      console.log('DOCUMENT');
+      console.log(documentResponse);
+
+      
+    } catch (error: any) {
+      if (error.code === 409) {
+        setLoading(false);
+        setAccError(true);
+      }
+    }
   };
 
   return (
     <main>
-      <div className='sm:grid sm:grid-cols-2'>
+      <div className='sm:grid sm:grid-cols-2 mb-8'>
         <Image
           src={DogPic}
           alt='dog picture'
@@ -162,7 +177,9 @@ export default function Login() {
                 width='32'
                 height='32'
                 viewBox='0 0 24 24'
-                className={`ml-2 ${loading ? 'block' : 'hidden'} absolute right-4 sm:left-1/2 sm:right-1/2`}
+                className={`ml-2 ${
+                  loading ? 'block' : 'hidden'
+                } absolute right-4 sm:left-1/2 sm:right-1/2`}
               >
                 <path
                   fill='currentColor'
@@ -177,6 +194,24 @@ export default function Login() {
                   />
                 </path>
               </svg>
+            </div>
+            <div
+              className={`${
+                accError ? 'flex' : 'hidden'
+              } gap-4 items-center bg-[--clr-accent] rounded-lg px-4 py-2 animate-notif`}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='32'
+                height='32'
+                viewBox='0 0 100 100'
+              >
+                <path
+                  fill='currentColor'
+                  d='m91.17 81.374l.006-.004l-.139-.24c-.068-.128-.134-.257-.216-.375l-37.69-65.283c-.611-1.109-1.776-1.87-3.133-1.87c-1.47 0-2.731.887-3.285 2.153l-.004-.002L9.312 80.529l.036.021a3.553 3.553 0 0 0-.82 2.257a3.59 3.59 0 0 0 3.588 3.59h75.767a3.59 3.59 0 0 0 3.589-3.589c0-.511-.11-.994-.302-1.434zm-41.135-1.757c-2.874 0-5.201-2.257-5.201-5.13c0-2.874 2.326-5.2 5.201-5.2c2.803 0 5.13 2.325 5.13 5.2a5.123 5.123 0 0 1-5.13 5.13zm5.13-45.367v28.299h-.002l.002.016c0 1.173-.95 2.094-2.094 2.094l-.014-.001v.001h-6.093c-1.174 0-2.123-.921-2.123-2.094l.002-.016h-.002V34.326c-.001-.026-.008-.051-.008-.077c0-1.117.865-1.996 1.935-2.078v-.016h6.288v.001c1.149.007 2.074.897 2.103 2.039h.005v.055h.001z'
+                />
+              </svg>
+              <h1>This email is already in use!</h1>
             </div>
           </div>
         </form>
