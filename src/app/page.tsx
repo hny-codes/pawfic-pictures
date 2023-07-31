2a07da759de398102c7bccd2d00ed250bd0e4ba9
Henry <hnguyhenry@protonmail.com>
2023-07-31 11:50:07 -0700

    Add link to account creation and login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,15 @@ export default function Home() {
         />
         <div className='text-[--clr-text] text-center mt-8 px-8 sm:place-self-center sm:text-left'>
           <h1 className='italic text-3xl sm:text-6xl'>Love Dogs?</h1>
-          <h2 className='text-sm sm:text-lg mt-4'>We do too! Get your dog dosage here</h2>
+          <h2 className='text-sm sm:text-lg mt-4'>
+            We do too! Get your dog dosage here
+          </h2>
           <Link href={'/login'}>
+            <button className='mt-8 ml-8 bg-[--clr-primary-01] px-4 py-2 rounded-lg hover:opacity-80 transition'>
+              Login here!
+            </button>
+          </Link>
+          <Link href={'/create'}>
             <button className='mt-8 bg-[--clr-primary-01] px-4 py-2 rounded-lg hover:opacity-80 transition'>
               Join us!
             </button>
