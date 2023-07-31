19deeb43ae66f1ee57ac341771c212462d994444
Henry <hnguyhenry@protonmail.com>
2023-07-31 11:22:46 -0700

    Add handling for invalid credentials

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,7 +22,7 @@ export default function Login() {
   const router = useRouter();
   const { user, setUser }: any = useAuth();
   const [loading, setLoading] = useState(false);
-  const [pwError, setPwError] = useState(false);
+  const [invalidError, setInvalidError] = useState(false);
 
   const {
     register,
@@ -45,7 +45,12 @@ export default function Login() {
         redirect('/home');
       }
     } catch (error) {
-      console.error(error);
+      console.error((error as any).code);
+      setLoading(false);
+      setInvalidError(true);
+      setTimeout(() => {
+        setInvalidError(false);
+      }, 5000);
     }
   };
 
@@ -60,7 +65,7 @@ export default function Login() {
           />
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className='text-[--clr-text] text-center mt-8 px-8 sm:place-self-center sm:text-left'
+            className='text-[--clr-text] text-center mt-8 px-8 sm:place-self-center sm:text-left relative'
           >
             <h1 className='text-3xl'>Login</h1>
             <div className='flex flex-col mt-8 max-w-[15rem] sm:w-full mx-auto'>
@@ -137,6 +142,24 @@ export default function Login() {
                   </path>
                 </svg>
               </div>
+            </div>
+            <div
+              className={`${
+                invalidError ? 'flex' : 'hidden'
+              } gap-4 items-center bg-[--clr-accent] rounded-lg px-4 py-2 animate-notif absolute left-0 right-0 mx-6 -top-20 sm:top-auto sm:-bottom-24`}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='32'
+                height='32'
+                viewBox='0 0 100 100'
+              >
+                <path
+                  fill='currentColor'
+                  d='m91.17 81.374l.006-.004l-.139-.24c-.068-.128-.134-.257-.216-.375l-37.69-65.283c-.611-1.109-1.776-1.87-3.133-1.87c-1.47 0-2.731.887-3.285 2.153l-.004-.002L9.312 80.529l.036.021a3.553 3.553 0 0 0-.82 2.257a3.59 3.59 0 0 0 3.588 3.59h75.767a3.59 3.59 0 0 0 3.589-3.589c0-.511-.11-.994-.302-1.434zm-41.135-1.757c-2.874 0-5.201-2.257-5.201-5.13c0-2.874 2.326-5.2 5.201-5.2c2.803 0 5.13 2.325 5.13 5.2a5.123 5.123 0 0 1-5.13 5.13zm5.13-45.367v28.299h-.002l.002.016c0 1.173-.95 2.094-2.094 2.094l-.014-.001v.001h-6.093c-1.174 0-2.123-.921-2.123-2.094l.002-.016h-.002V34.326c-.001-.026-.008-.051-.008-.077c0-1.117.865-1.996 1.935-2.078v-.016h6.288v.001c1.149.007 2.074.897 2.103 2.039h.005v.055h.001z'
+                />
+              </svg>
+              <h1>Sorry! Invalid email or password.</h1>
             </div>
             <Link href={'/'}>
               <p className='hover:text-[--clr-primary-01] transition'>
