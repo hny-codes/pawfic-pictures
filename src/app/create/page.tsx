7e239b24900faab2b78e83308281fadbbe9d8c1b
Henry <hnguyhenry@protonmail.com>
2023-07-31 11:28:35 -0700

    Move account error message to separate component

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -14,6 +14,7 @@ import {
 } from '@/appwriteConfig';
 import { ID } from 'appwrite';
 import { useAuth } from '@/utils/AuthContext';
+import InUseMessage from './components/InUseMessage';
 
 interface FormInput {
   email: string;
@@ -30,7 +31,7 @@ export default function Create() {
   const router = useRouter();
   const { user, setUser }: any = useAuth();
   const [loading, setLoading] = useState(false);
-  const [accError, setAccError] = useState(false);
+  const [accError, setAccError] = useState(true);
 
   const {
     register,
@@ -210,24 +211,7 @@ export default function Create() {
                   </path>
                 </svg>
               </div>
-              <div
-                className={`${
-                  accError ? 'flex' : 'hidden'
-                } gap-4 items-center bg-[--clr-accent] rounded-lg px-4 py-2 animate-notif`}
-              >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='32'
-                  height='32'
-                  viewBox='0 0 100 100'
-                >
-                  <path
-                    fill='currentColor'
-                    d='m91.17 81.374l.006-.004l-.139-.24c-.068-.128-.134-.257-.216-.375l-37.69-65.283c-.611-1.109-1.776-1.87-3.133-1.87c-1.47 0-2.731.887-3.285 2.153l-.004-.002L9.312 80.529l.036.021a3.553 3.553 0 0 0-.82 2.257a3.59 3.59 0 0 0 3.588 3.59h75.767a3.59 3.59 0 0 0 3.589-3.589c0-.511-.11-.994-.302-1.434zm-41.135-1.757c-2.874 0-5.201-2.257-5.201-5.13c0-2.874 2.326-5.2 5.201-5.2c2.803 0 5.13 2.325 5.13 5.2a5.123 5.123 0 0 1-5.13 5.13zm5.13-45.367v28.299h-.002l.002.016c0 1.173-.95 2.094-2.094 2.094l-.014-.001v.001h-6.093c-1.174 0-2.123-.921-2.123-2.094l.002-.016h-.002V34.326c-.001-.026-.008-.051-.008-.077c0-1.117.865-1.996 1.935-2.078v-.016h6.288v.001c1.149.007 2.074.897 2.103 2.039h.005v.055h.001z'
-                  />
-                </svg>
-                <h1>This email is already in use!</h1>
-              </div>
+              <InUseMessage accError={accError} />
             </div>
             <Link href={'/'}>
               <p className='hover:text-[--clr-primary-01] transition'>
