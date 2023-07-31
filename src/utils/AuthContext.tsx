02539eab89629f03d9481e60188553899d8ff2f8
Henry <hnguyhenry@protonmail.com>
2023-07-31 10:47:31 -0700

    Add sustain user session function

--- src/utils/AuthContext.tsx
+++ src/utils/AuthContext.tsx
@@ -1,33 +1,63 @@
 'use client';
 
-import { createContext, useState, useEffect, useContext } from 'react';
+import {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  Dispatch,
+  SetStateAction,
+} from 'react';
+import { account } from '@/appwriteConfig';
+import { Models } from 'appwrite';
 
-export type User = {
-  user: boolean;
+// Context Type
+type UserContextType = {
+  user: null | Models.User<Models.Preferences>;
+  setUser: Dispatch<SetStateAction<Models.User<Models.Preferences> | null>>;
 };
 
-export const AuthContext = createContext<null | User>(null);
+// Create auth context
+export const AuthContext =
+  createContext<null | Models.User<Models.Preferences>>(null);
 
+// Create exportable provider
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState<null | Models.User<Models.Preferences>>(
+    null
+  );
 
   useEffect(() => {
-    setLoading(false);
+    getUserOnLoad();
   }, []);
 
-  const contextData = {
+  const getUserOnLoad = async () => {
+    try {
+      const accountDetails = await account.get();
+      setUser(accountDetails);
+    } catch (error) {
+      if ((error as any).code === 401) {
+        console.error('No login session available!');
+      }
+    }
+    setLoading(false);
+  };
+
+  const contextData: UserContextType = {
     user,
     setUser,
   };
 
   return (
+    // @ts-ignore
     <AuthContext.Provider value={contextData}>
-      {loading ? <p>loading..</p> : children}
+      {loading ? <p className='text-white'>loading..</p> : children}
     </AuthContext.Provider>
   );
 };
 
+// Hook to access auth data
 export const useAuth = () => {
   return useContext(AuthContext);
 };
